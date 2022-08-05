04a8fa329035ff4c82692c32e4bfe2d421627b29
FlutLab.io <>
2022-08-05 13:23:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -24,29 +24,33 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Kindacode.com"),
-        ),
-        body: Stack(
-          children: [
-            Image.network('https://www.kindacode.com/wp-content/uploads/2020/10/sample.jpg', width: double.infinity, height: 250, fit: BoxFit.cover),
-            Positioned(
-              // The Positioned widget is used to position the text inside the Stack widget
-              bottom: 10,
-              right: 10,
+      appBar: AppBar(
+        title: const Text("Kindacode.com"),
+      ),
+      body: Stack(
+        children: [
+          Image.network('https://www.kindacode.com/wp-content/uploads/2020/10/sample.jpg', width: double.infinity, height: 250, fit: BoxFit.cover),
+          Positioned(
+            // The Positioned widget is used to position the text inside the Stack widget
+            bottom: 10,
+            right: 10,
 
-              child: Container(
-                // We use this Container to create a black box that wraps the white text so that the user can read the text even when the image is white
-                width: 300,
-                color: Colors.black54,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'I Like Potatoes And Oranges',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
+            child: Container(
+              // We use this Container to create a black box that wraps the white text so that the user can read the text even when the image is white
+              width: 300,
+              color: Colors.black54,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'I Like Potatoes And Oranges',
+                style: TextStyle(fontSize: 20, color: Colors.white),
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+        ],
+      ),
+    );
+
+    const SizedBox(height: 20);
+    Image.asset('assets/dessous.png', width: double.infinity, height: 250, fit: BoxFit.cover);
   }
 }
